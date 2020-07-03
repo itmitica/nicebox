9f6e7245955c90615890a5e1e600901d37631711
Dumitru UNGUREANU <itmitica@gmail.com>
2020-07-03 23:40:19 +0300

    no compton cleanup

--- bin/apps.sh
+++ bin/apps.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-apt install blueman clamav clamav-daemon clamtk compton feh galculator git gucharmap gufw intel-microcode htop links2 mpv numlockx pavucontrol pulseaudio-module-bluetooth redshift rofi vivaldi vim-gtk vifm xfce4-power-manager xfce4-terminal wicd wicd-curses wicd-cli -y
+apt install blueman clamav clamav-daemon clamtk feh galculator git gucharmap gufw intel-microcode htop links2 mpv numlockx pavucontrol pulseaudio-module-bluetooth redshift rofi vivaldi vim-gtk vifm xfce4-power-manager xfce4-terminal wicd wicd-curses wicd-cli -y
